81fc010ffb47f8619d6a2897fec6ac584e7e380b
oscarzhao <oscarzhaosl@gmail.com>
2017-03-09 20:12:18 +0800

    todo app

---
+++ learnyouahaskell/todo.hs
@@ -0,0 +1,44 @@
+-- todo
+-- 1. view tasks
+-- 2. add tasks
+-- 3. delete tasks
+
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: [(String, [String] -> IO())]
+dispatch = [ ("add", add)
+  , ("view", view)
+  , ("remove", remove)
+  ]
+
+add :: [String] -> IO()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO()
+remove [fileName, itemNumber] = do
+  handle <- openFile fileName ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  contents <- hGetContents handle
+  let number = read itemNumber
+      todoTasks = lines contents
+      newTodoItems = delete (todoTasks !! number) todoTasks
+  hPutStr tempHandle $ unlines newTodoItems
+  hClose handle
+  hClose tempHandle
+  removeFile fileName
+  renameFile tempName fileName
+
+main = do
+  (command: args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
