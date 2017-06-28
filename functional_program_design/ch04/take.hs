fae7881d48336cfca6593e3824579718a8b1d2d0
oscarzhao <oscarzhaosl@gmail.com>
2017-06-28 22:34:51 +0800

    implement splitAt

--- functional_program_design/ch04/take.hs
+++ functional_program_design/ch04/take.hs
@@ -8,8 +8,16 @@
 -- result is that it's impossible
 -- Why:  ?
 
+-- mytake does not satisfy the above conditions
 mytake _ [] = []
 mytake 0 _ = []
 mytake n (x:xs)
     | n <= 0     = []
     | otherwise  = x: (mytake (n-1) xs)
+
+-- implement splitAt which run over in one iteration
+mySplitAt :: Int -> [a] -> ([a], [a])
+mySplitAt _ []     = ([],[])
+mySplitAt n (x:xs) =  if n <= 0  then ([], x:xs)
+                    else (x:first, second)
+                    where (first, second) = mySplitAt (n-1) xs
