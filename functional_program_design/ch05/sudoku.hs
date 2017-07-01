35f8db163234bb48e6c39701227c071a80581031
oscarzhao <oscarzhaosl@gmail.com>
2017-07-02 01:26:35 +0800

    implement choices function for sudoku

--- functional_program_design/ch05/sudoku.hs
+++ functional_program_design/ch05/sudoku.hs
@@ -2,7 +2,6 @@
 -- Chapter 05 solve Sudoku
 --
 
-
 import Data.List
 
 -- Data Types
@@ -25,13 +24,23 @@ blank = (=='0')
 solve :: Grid -> [Grid]
 solve = filter valid . completions
 
-
 -- completions fill blanks in a Grid, and produces all possible Grids
 completions :: Grid -> [Grid]
 completions = expand . choices
 
 choices :: Grid -> Matrix [Digit]
-choices g = []
+choices m = zipWith rowCandidate [0..] m
+            where candidate idxR idxC y = if blank y then (digits \\ (union (union (exR!!idxR) (exC!!idxC)) (exB!!(unitNum idxR idxC))) ) else [y]
+                  rowCandidate idxR xs = zipWith (candidate idxR) [0..] xs
+                  (exR, exC, exB) = exclude m
+
+-- get exclusions for rows, cols, blocks
+exclude :: Grid -> ([Row Digit], [Row Digit], [Row Digit])
+exclude m = (r, c, b)
+            where excludeGrid = map (nub . foldl (\ys y -> if blank y then ys else (y:ys)) [])
+                  r = excludeGrid m
+                  c = excludeGrid (transpose m)
+                  b = excludeGrid (unitGrid m)
 
 expand :: Matrix [Digit] -> [Grid]
 expand _ = []
@@ -48,6 +57,18 @@ valid m = and [blocksValid, rowsValid, colsValid]
 unitGrid :: Grid -> [Row Digit]
 unitGrid = map concat . concat . map (splitGrid . transpose) . splitGrid
 
+-- unitNum get the index of the block(unit) in a 9*9 Grid
+unitNum :: Int -> Int -> Int
+unitNum row col 
+        | row < 3 && col < 3 = 0
+        | row < 3 && col < 6 = 1
+        | row < 3 = 2
+        | row < 6 && col < 3 = 3
+        | row < 6 && col < 6 = 4
+        | row < 6 = 5
+        | col < 3 = 6
+        | col < 6 = 7
+        | otherwise = 8
 
 -- splitList splits a list with length n into n/3 list with 3 elements
 splitList :: Int -> [a] -> [[a]]
@@ -55,6 +76,7 @@ splitList interval g = reverse (map reverse (foldl appendX [] g))
             where appendX [] x = [[x]]
                   appendX (xs:xss) x = if length xs < interval then (x:xs):xss
                                        else [x]:xs:xss
+
 -- splitGrid splits a 9*9 Grid into 3 3*9 Grids
 splitGrid :: Grid -> [Grid]
 splitGrid = splitList 3
@@ -64,4 +86,5 @@ validRow xs = snd (foldl acc ('0', True) xs)
               where acc (prev, False) x = (x, False) -- duplication found
                     acc (prev, True) x = (x, x /= prev)
 
-sample = replicate 9 digits
+-- sample.json is all the possibilities of Grid
+sample = ["004005700", "000009400", "360000008", "720060000", "000402000", "000080093", "400000056", "005300000", "006100900"]
