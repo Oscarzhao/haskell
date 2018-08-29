c252816f74a534615b8f943ddf44de74704b8c77
shuailong shuailong.zhao <shuailong.zhao@grabtaxi.com>
2018-08-29 18:56:42 +0800

    memory use

--- functional_program_design/ch07_performance/memory_use.hs
+++ functional_program_design/ch07_performance/memory_use.hs
@@ -7,3 +7,32 @@
 -- > foldr (+) 0 [1..1000000]
 -- > 500000500000
 -- > (0.14 secs, 161,632,384 bytes)
+
+import Data.List
+
+-- mean , sumlen1
+-- sumlen1 is the easist to implement, but with performance issue
+sumlen1 xs = (sum xs, length xs)
+
+-- sumlen2
+sumlen2 [] = (0, 0)
+sumlen2 xs = foldr f (0,0) xs where f x (s, n) = (s+x, n+1)
+
+-- sumlen3
+sumlen3 [] = (0, 0)
+sumlen3 xs = foldl' g (0,0) xs where g (s, n) x = (s+x, n+1)
+
+-- sumlen4 : O(n) memory
+sumlen4 :: [Float] -> (Float, Int)
+sumlen4 = foldl' g (0,0) where g (s, n) x = s `seq` n `seq` (s+x, n+1)
+
+-- Cartesian product
+-- sum $ map sum $ cp1 [[1..10] | j <- [1..6]]
+cp1 [] = [[]]
+cp1 (xs:xss) = [x:ys | x <- xs, ys <- cp1 xss]
+
+-- cp2, performance better
+-- sum $ map sum $ cp2 [[1..10] | j <- [1..6]]
+cp2 :: [[a]] -> [[a]]
+cp2 = foldr op [[]]
+  where op xs yss = [x:ys | x <- xs, ys <- yss]
--- functional_program_design/ch07_performance/sum_primes.hs
+++ functional_program_design/ch07_performance/sum_primes.hs
@@ -1,27 +1,29 @@
 -- compare the performance of foo1 and foo2
 -- use `:set +s` to enable statistics
-
 -- every time, foo1 would need the same calculation for the same input
 -- because primes is binded with the use of foo1, not function foo1
+-- foo1 has no cache
 foo1 n = sum (take n primes)
   where
-    primes     = [x | x <- [2..], divisors x == [x]]
-    divisors x = [d | d <- [2..x], x `mod` d == 0]
+    primes = [x | x <- [2 ..], divisors x == [x]]
+    divisors x = [d | d <- [2 .. x], x `mod` d == 0]
 
 -- foo2 : primes would cache results after the first calculation
 -- so that later calculation would be quite fast
 -- but it cost a lot of memory
 -- here primes has no dependency on foo2's input
 foo2 n = sum (take n primes)
-primes     = [x | x <- [2..], divisors x == [x]]
-divisors x = [d | d <- [2..x], x `mod` d == 0]
+
+primes = [x | x <- [2 ..], divisors x == [x]]
+
+divisors x = [d | d <- [2 .. x], x `mod` d == 0]
 
 -- foo3 : primes would cache
 -- because primes is independent with foo3's input
 foo3 = sum . flip take primes
-    where
-      primes     = [x | x <- [2..], divisors x == [x]]
-      divisors x = [d | d <- [2..x], x `mod` d == 0]
+  where
+    primes = [x | x <- [2 ..], divisors x == [x]]
+    divisors x = [d | d <- [2 .. x], x `mod` d == 0]
 
 -- test with commands (foo2 would cache primes and divsiors)
 -- > foo1 1000
